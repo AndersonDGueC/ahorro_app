bb754576d5953ffc1bb5442ce62de8405bce25d1
anderson <adguerreroc@unal.edu.co>
2020-11-14 16:57:37 -0500

    parte metas finalizada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Title_name from './components/header/Title_name'
 import User_balance from './components/body/User_balance'
+import Metas from './components/body/Metas'
 function App() {
   return (
     <div className="App">
         <Title_name/>
         <User_balance/>
+        <Metas/>
     </div>
   );
 }
---
+++ src/components/body/Metas.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import {CgAdd} from 'react-icons/cg'
+import {IconContext} from 'react-icons'
+import palmera from './images/IMG0.png'
+import computador from './images/IMG2.png'
+const Metas=()=>{
+return(
+    <section id="metas1">
+        <article className="container-fluid">
+        <div className="row">
+        <div id="title_meta" className="col">
+        Tus metas
+        </div>
+        <div className="col">
+        <IconContext.Provider value={{size:'25px',color:'#54D1ED'}}>
+                   <div id="add_intitle" >
+                <CgAdd/>
+                  </div> 
+                    </IconContext.Provider>
+        </div>
+        <div>
+
+        </div>
+        </div>
+        </article>
+        <article className="container-fluid">
+        <div className="row">
+        <div id="sec_palm" className="col">
+        <figure>
+            <img id="palm" src={palmera} alt="palmera"></img>
+        </figure>
+        <p id="paragraph4">Viaje a  Islas Canarias</p>
+        <div className="container-fluid">
+        <div className="row">
+        <div id="sec_porcen" className="col">
+        
+            <p id="number_porcen">20%</p>
+        
+        </div>
+        <div className="col">
+        <p id="bono">$1.630.000</p>
+        <p id="real_bon">$8.000.000</p>
+        </div>
+        </div>
+        </div>
+        </div>
+        <div id="sec_compu" className="col">
+        <figure>
+            <img id="compu" src={computador} alt='computador'></img>
+        </figure>
+        <p id="paragraph5">Macbook Pro</p>
+        <div className="container-fluid">
+        <div className="row">
+        <div id="sec2_porcen" className="col">
+        
+            <p id="number_porcen2">0%</p>
+        
+        </div>
+        <div className="col">
+        <p id="bono2">$0</p>
+        <p id="real_bon2">$7.000.000</p>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </article>
+    </section>
+)
+}
+
+export default Metas
